523170ab8ff0199d4f7c5cb3371730aa10f32181
foxidokun <foxido@foxido.dev>
2024-10-08 23:07:44 +0300

    blurring out

--- Assets/TileLogic.cs
+++ Assets/TileLogic.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class TileLogic : MonoBehaviour
 {
+    const int MAX_DEAD_PHASE = 3;
+
     Material my_material_;
     private State _cur_state;
+    private int dead_phase = MAX_DEAD_PHASE;
 
     public State CurState {
         get => _cur_state;
@@ -14,8 +18,12 @@ public class TileLogic : MonoBehaviour
             _cur_state = value;
             if (value == State.Alive) {
                 my_material_.color = new Color(1.0f, 1.0f, 1.0f, 1);
+                dead_phase = 0;
             } else if (value == State.Dead) {
-                my_material_.color = new Color(0.1f, 0.1f, 0.1f, 1);
+                float color_x = 0.01f + 0.6f * (MAX_DEAD_PHASE - dead_phase) / MAX_DEAD_PHASE;
+                my_material_.color = new Color(color_x, color_x, color_x, 1);
+
+                if (dead_phase < MAX_DEAD_PHASE) dead_phase++;
             }
         }
     }
